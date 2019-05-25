cc0e9047e0ca12f410cea195ca97c550d4476330
Henry Gao <Hgao135@gmail.com>
2019-05-25 19:38:51 -0400

    Move

---
+++ foodman/Models/FoodCat.swift
@@ -0,0 +1,16 @@
+//
+//  FoodCat.swift
+//  foodman
+//
+//  Created by Henry Gao on 5/25/19.
+//  Copyright © 2019 foodman corp. All rights reserved.
+//
+
+import Foundation
+
+class food {var strCategory: String?}
+class meals {var meals: [food]}
+
+class FoodCat {
+    // class definition goes here
+}
