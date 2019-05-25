79cc0a8b4d38237ce6789d5f12f503974c706b6c
Hackathon <jansenw95@gmail.com>
2019-05-25 18:41:04 -0400

    added search bar

---
+++ foodman/view/searchViewController.swift
@@ -0,0 +1,31 @@
+//
+//  searchViewController.swift
+//  foodman
+//
+//  Created by Jansen Wong on 5/25/19.
+//  Copyright © 2019 foodman corp. All rights reserved.
+//
+
+import UIKit
+
+class searchViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
